26bbdef45d63ef06c06cfba3254d48af533d023a
Ashwin Kumar <106005097+an-ash-iith@users.noreply.github.com>
2024-06-07 00:56:19 +0530

    Add files via upload

---
+++ Leetcode daily problem/append_character.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        
+        int n = s.size();
+        int m = t.size();
+        
+        int j =0;
+
+        for(int i =0;i<n;i++)
+        {
+            if(j==m)
+            return 0;
+
+            if(s[i]==t[j])
+            {
+                j++;
+            }
+        }
+
+        return m-j;
+    }
+};
