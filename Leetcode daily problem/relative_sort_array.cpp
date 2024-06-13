faf92336871b9932e38283dc79979c9282cd4afc
Ashwin Kumar <106005097+an-ash-iith@users.noreply.github.com>
2024-06-13 21:16:42 +0530

    Add files via upload

---
+++ Leetcode daily problem/relative_sort_array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        
+        map<int,int> mp;
+
+        for(int i =0;i<arr1.size();i++)
+        {
+            mp[arr1[i]]++;
+        }
+
+        vector<int> ans;
+
+        for(int i =0;i<arr2.size();i++)
+        {
+            while(mp[arr2[i]]!=0)
+            {
+                ans.push_back(arr2[i]);
+                mp[arr2[i]]--;
+            }
+        }
+
+     for(auto it: mp)
+     {
+        while(it.second>0)
+        {ans.push_back(it.first);
+         it.second--;
+        }
+     }
+
+        return ans;
+    }
+};
