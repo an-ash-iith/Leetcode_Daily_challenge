b4f64e558cb6babbc79c68ebe64c50d57d26f950
Ashwin Kumar <106005097+an-ash-iith@users.noreply.github.com>
2024-06-13 21:12:27 +0530

    Add files via upload

---
+++ Leetcode daily problem/height_checker.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        
+        vector<int>ans;
+        ans = heights;
+
+        sort(ans.begin(),ans.end());
+
+         int count =0;
+
+        for(int i =0;i<heights.size();i++)
+        {
+            if(ans[i]!=heights[i]) count++;
+        }
+
+        return count;
+    }
+};
