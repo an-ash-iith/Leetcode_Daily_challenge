cacb17d95d9299f24e008ab5b1c3cc52087f405e
Ashwin Kumar <106005097+an-ash-iith@users.noreply.github.com>
2024-06-13 21:18:06 +0530

    Add files via upload

---
+++ Leetcode daily problem/number_of_moves.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        
+        //it is soritng and two pointer
+
+        sort(seats.begin(),seats.end());
+        sort(students.begin(),students.end());
+
+        int n = seats.size();
+        int ans =0;
+
+        for(int i =0;i<n;i++)
+        {
+         ans+=abs(seats[i]-students[i]);
+        }
+
+        return ans;
+    }
+};
